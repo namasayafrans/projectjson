6381811bac70db4011ec14665850338a183b18eb
namasayafrans <sinagafrans38@gmail.com>
2024-03-22 19:48:32 +0700

    projectjson

---
+++ UMKMapps/app/src/main/java/com/example/umkmapps/model/ResponseUser.kt
@@ -0,0 +1,22 @@
+package com.example.umkmapps.model
+
+import android.graphics.pdf.PdfDocument.Page
+import com.google.gson.annotations.SerializedName
+
+data class ResponseUser(
+
+        @field:SerializedName("page")
+        val page: Int? = null,
+
+        @field:SerializedName("per_page")
+        val perPage: Int? = null,
+
+        @field:SerializedName("total")
+        val total: Int? = null,
+
+        @field:SerializedName("total_pages")
+        val totalPages: Int? = null,
+
+        @field:SerializedName("data")
+        val data: List<DataItem>? = null
+)
